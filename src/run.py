1cd02aac46b599e20759bebe26aa3c68e475a423
hecongqing <1101908732@qq.com>
2019-10-27 23:21:53 +0800

    update

---
+++ src/run.py
@@ -0,0 +1,16 @@
+import das.tensorflow.estimator as te
+t_job= te.TensorFlow(entry_point = '/mnt/hecongqing1/cail/src/main6593.py', \
+                       train_gpu_count = 1,
+                       hyperparameters = {'task_name':"task3","do_train":True, "train_path":"../input/SCM_5k.json",
+                                          "do_eval":False, "do_predict":False, "vocab_file":"./chinese_L-12_H-768_A-12/vocab.txt",
+                                          "bert_config_file":"./chinese_L-12_H-768_A-12/bert_config.json",
+                                          "init_checkpoint":"./chinese_L-12_H-768_A-12/bert_model.ckpt",
+                                          "save_checkpoints_steps":500,"max_seq_length":512,"train_batch_size":16,
+                                          "learning_rate":2e-5,"num_train_epochs":10.0,
+                                          "output_dir":"../output/main6593/"}
+                      )
+
+
+
+t_job.fit(base_job_name='test',
+         )
